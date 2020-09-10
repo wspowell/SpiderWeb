d4ad9e75a2052f5b6096e17233e043b810a815e0
Wesley Powell <wesley.scott.powell@gmail.com>
2020-09-10 00:39:42 -0500

    Made Context the main type

--- local/localizer.go
+++ local/localizer.go
@@ -49,24 +49,25 @@ func (self *contextualized) setContext(context context.Context) {
 	self.context = context
 }
 
-// Context is a alias for Localizer.
-// Using local.Context may read better and help associated it with context.Context.
-type Context Localizer
-
 // Localizer stores data local to a goroutine. A localized context.
 // This works differently than context.Context in that it is not meant to
 //   cross API boundaries and is not immutable. However, it is designed to
 //   be able to work alongside context.Context. It is also meant to be
 //   wrapped by developers to allow for direct access of endpoint local data.
-// Localizer is also a context.Context. It is expected that the value of
-//   the internal context.Context is thread safe.
 // Not thread safe.
 type Localizer interface {
-	contextualizer
 	Localize(key interface{}, value interface{})
 }
 
-var _ Localizer = (*Localized)(nil)
+// Context is a contextualizer and a Localizer.
+// Context is meant to be used as a replacement for context.Context.
+// It is expected that the value of the internal context.Context is thread safe.
+type Context interface {
+	contextualizer
+	Localizer
+}
+
+var _ Context = (*Localized)(nil)
 
 type Localized struct {
 	contextualized
@@ -101,13 +102,13 @@ func (self *Localized) Localize(key interface{}, value interface{}) {
 	self.locals[key] = value
 }
 
-func WithValue(parent Localizer, key interface{}, value interface{}) {
+func WithValue(parent Context, key interface{}, value interface{}) {
 	childContext := context.WithValue(parent.Context(), key, value)
 
 	parent.setContext(childContext)
 }
 
-func WithCancel(parent Localizer) context.CancelFunc {
+func WithCancel(parent Context) context.CancelFunc {
 	childContext, cancel := context.WithCancel(parent.Context())
 
 	parent.setContext(childContext)
@@ -115,7 +116,7 @@ func WithCancel(parent Localizer) context.CancelFunc {
 	return cancel
 }
 
-func WithDeadline(parent Localizer, deadline time.Time) context.CancelFunc {
+func WithDeadline(parent Context, deadline time.Time) context.CancelFunc {
 	childContext, cancel := context.WithDeadline(parent.Context(), deadline)
 
 	parent.setContext(childContext)
@@ -123,7 +124,7 @@ func WithDeadline(parent Localizer, deadline time.Time) context.CancelFunc {
 	return cancel
 }
 
-func WithTimeout(parent Localizer, timeout time.Duration) context.CancelFunc {
+func WithTimeout(parent Context, timeout time.Duration) context.CancelFunc {
 	childContext, cancel := context.WithTimeout(parent.Context(), timeout)
 
 	parent.setContext(childContext)
