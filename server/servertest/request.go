eee5e8ee382fb4f8529c59f9c758cfee1c27e397
Wesley Powell <wesley.scott.powell@gmail.com>
2021-05-08 14:08:57 +0000

    Improved usage of request tests

--- server/servertest/request.go
+++ server/servertest/request.go
@@ -18,7 +18,24 @@ type Mocker interface {
 	AssertExpectations(t mock.TestingT) bool
 }
 
+type testCase struct {
+	t *testing.T
+
+	server *server.Server
+	name   string
+}
+
+func TestCase(t *testing.T, server *server.Server, name string) *testCase {
+	return &testCase{
+		t:      t,
+		server: server,
+		name:   name,
+	}
+}
+
 type requestTestCase struct {
+	*testCase
+
 	httpMethod      string
 	path            string
 	requestMimeType string
@@ -30,8 +47,9 @@ type requestTestCase struct {
 }
 
 // GivenRequest starts a request test case to be provided to TestRequest.
-func GivenRequest(httpMethod string, path string) *requestTestCase {
+func (self *testCase) GivenRequest(httpMethod string, path string) *requestTestCase {
 	return &requestTestCase{
+		testCase:      self,
 		httpMethod:    httpMethod,
 		path:          path,
 		headers:       map[string]string{},
@@ -71,6 +89,7 @@ func (self *requestTestCase) WithResourceMock(resource string, mock Mocker) *req
 
 func (self *requestTestCase) ExpectResponse(httpStatus int) *responseTestCase {
 	return &responseTestCase{
+		testCase:   self.testCase,
 		request:    self,
 		httpStatus: httpStatus,
 		headers:    map[string]string{},
@@ -78,6 +97,8 @@ func (self *requestTestCase) ExpectResponse(httpStatus int) *responseTestCase {
 }
 
 type responseTestCase struct {
+	*testCase
+
 	request *requestTestCase
 
 	httpStatus       int
@@ -104,119 +125,124 @@ func (self *responseTestCase) WithResponseBody(mimeType string, responseBody []b
 	return self
 }
 
-// TestRequest for request/response roundtrip.
-func TestRequest(t *testing.T, server *server.Server, about string, testCase *responseTestCase) {
-	t.Run(about, func(t *testing.T) {
-		copyRequestBody := make([]byte, len(testCase.request.requestBody))
-		copy(copyRequestBody, testCase.request.requestBody)
-
-		copyResponseBody := make([]byte, len(testCase.responseBody))
-		copy(copyResponseBody, testCase.responseBody)
-
-		copyTestCase := responseTestCase{
-			request: &requestTestCase{
-				httpMethod:      testCase.request.httpMethod,
-				path:            testCase.request.path,
-				requestMimeType: testCase.request.requestMimeType,
-				requestBody:     copyRequestBody,
-				headers:         testCase.request.headers,
-				queryParams:     testCase.request.queryParams,
-				pathParams:      testCase.request.pathParams,
-				resourceMocks:   testCase.request.resourceMocks,
-			},
-
-			httpStatus:       testCase.httpStatus,
-			responseMimeType: testCase.responseMimeType,
-			responseBody:     copyResponseBody,
-			headers:          testCase.headers,
-			emptyBody:        testCase.emptyBody,
-		}
-
-		url := copyTestCase.request.path
-		for param, value := range copyTestCase.request.pathParams {
-			url = strings.Replace(url, "{"+param+"}", value, 1)
-		}
-
-		var req fasthttp.Request
-
-		req.Header.SetMethod(copyTestCase.request.httpMethod)
-		req.Header.SetRequestURI(url)
-		req.Header.Set(fasthttp.HeaderHost, "localhost")
-
-		if copyTestCase.request.requestMimeType == "" && copyTestCase.responseMimeType == "" {
-			copyTestCase.request.requestMimeType = "application/json"
-			copyTestCase.responseMimeType = "application/json"
-		} else if copyTestCase.responseMimeType == "" {
-			copyTestCase.responseMimeType = copyTestCase.request.requestMimeType
-		}
-		req.Header.Set("Content-Type", copyTestCase.request.requestMimeType)
-		req.Header.Set("Accept", copyTestCase.responseMimeType)
-
-		for header, value := range copyTestCase.request.headers {
-			req.Header.Set(header, value)
-		}
-
-		req.SetBody(copyTestCase.request.requestBody)
-
-		requestCtx := fasthttp.RequestCtx{}
-		requestCtx.Init(&req, nil, nil)
-
-		// Setup mock calls.
-		endpoint := server.Endpoint(copyTestCase.request.httpMethod, copyTestCase.request.path)
-		originalResources := map[string]interface{}{}
-		if endpoint != nil {
-			for name, resource := range endpoint.Config.Resources {
-				originalResources[name] = resource
-				if mock, ok := copyTestCase.request.resourceMocks[name]; ok {
-					endpoint.Config.Resources[name] = mock
-				} else {
-					// Do not call resources. Must be mocked.
-					endpoint.Config.Resources[name] = nil
-				}
-			}
-		}
-
-		actualHttpStatus, actualResponseBody := server.Execute(&requestCtx)
-
-		if endpoint != nil {
-			// Put the resources back.
-			for name, originalResource := range originalResources {
-				if mock, ok := copyTestCase.request.resourceMocks[name]; ok {
-					mock.AssertExpectations(t)
-				}
-				endpoint.Config.Resources[name] = originalResource
-			}
-		}
-
-		for header, value := range copyTestCase.headers {
-			actualHeaderValue := requestCtx.Response.Header.Peek(header)
-			if !bytes.Equal(actualHeaderValue, []byte(value)) {
-				t.Errorf("expected header %v = %v , but got %v = %v", header, value, header, actualHeaderValue)
-			}
-		}
-
-		if copyTestCase.httpStatus != actualHttpStatus {
-			t.Errorf("expected http status %v, but got %v", copyTestCase.httpStatus, actualHttpStatus)
-		}
-
-		if copyTestCase.emptyBody {
-			if !bytes.Equal(nil, actualResponseBody) {
-				t.Errorf("expected empty response body, but got '%v'", string(actualResponseBody))
-			}
-		} else {
-			if requestCtx.Response.Header.ContentType() == nil {
-				t.Errorf("response is missing header Content-Type")
-			} else if !bytes.Equal(requestCtx.Response.Header.ContentType(), []byte(copyTestCase.responseMimeType)) {
-				t.Errorf("expected response mime type '%v', but got '%v'", copyTestCase.responseMimeType, requestCtx.Response.Header.ContentType())
-			}
-
-			if !bytes.Equal(copyTestCase.responseBody, actualResponseBody) {
-				t.Errorf("expected response body '%v', but got '%v'", string(copyTestCase.responseBody), string(actualResponseBody))
-			}
-		}
-
-		requestFuzzTest(t, server, testCase.request.httpMethod, testCase.request.path)
+func (self *responseTestCase) Run() {
+	self.t.Run(self.name, func(t *testing.T) {
+		runTest(self)
 	})
+}
+
+func runTest(testCase *responseTestCase) {
+	t := testCase.t
+
+	copyRequestBody := make([]byte, len(testCase.request.requestBody))
+	copy(copyRequestBody, testCase.request.requestBody)
+
+	copyResponseBody := make([]byte, len(testCase.responseBody))
+	copy(copyResponseBody, testCase.responseBody)
+
+	copyTestCase := responseTestCase{
+		request: &requestTestCase{
+			httpMethod:      testCase.request.httpMethod,
+			path:            testCase.request.path,
+			requestMimeType: testCase.request.requestMimeType,
+			requestBody:     copyRequestBody,
+			headers:         testCase.request.headers,
+			queryParams:     testCase.request.queryParams,
+			pathParams:      testCase.request.pathParams,
+			resourceMocks:   testCase.request.resourceMocks,
+		},
+
+		httpStatus:       testCase.httpStatus,
+		responseMimeType: testCase.responseMimeType,
+		responseBody:     copyResponseBody,
+		headers:          testCase.headers,
+		emptyBody:        testCase.emptyBody,
+	}
+
+	url := copyTestCase.request.path
+	for param, value := range copyTestCase.request.pathParams {
+		url = strings.Replace(url, "{"+param+"}", value, 1)
+	}
+
+	var req fasthttp.Request
+
+	req.Header.SetMethod(copyTestCase.request.httpMethod)
+	req.Header.SetRequestURI(url)
+	req.Header.Set(fasthttp.HeaderHost, "localhost")
+
+	if copyTestCase.request.requestMimeType == "" && copyTestCase.responseMimeType == "" {
+		copyTestCase.request.requestMimeType = "application/json"
+		copyTestCase.responseMimeType = "application/json"
+	} else if copyTestCase.responseMimeType == "" {
+		copyTestCase.responseMimeType = copyTestCase.request.requestMimeType
+	}
+	req.Header.Set("Content-Type", copyTestCase.request.requestMimeType)
+	req.Header.Set("Accept", copyTestCase.responseMimeType)
+
+	for header, value := range copyTestCase.request.headers {
+		req.Header.Set(header, value)
+	}
+
+	req.SetBody(copyTestCase.request.requestBody)
+
+	requestCtx := fasthttp.RequestCtx{}
+	requestCtx.Init(&req, nil, nil)
+
+	// Setup mock calls.
+	endpoint := testCase.server.Endpoint(copyTestCase.request.httpMethod, copyTestCase.request.path)
+	originalResources := map[string]interface{}{}
+	if endpoint != nil {
+		for name, resource := range endpoint.Config.Resources {
+			originalResources[name] = resource
+			if mock, ok := copyTestCase.request.resourceMocks[name]; ok {
+				endpoint.Config.Resources[name] = mock
+			} else {
+				// Do not call resources. Must be mocked.
+				endpoint.Config.Resources[name] = nil
+			}
+		}
+	}
+
+	actualHttpStatus, actualResponseBody := testCase.server.Execute(&requestCtx)
+
+	if endpoint != nil {
+		// Put the resources back.
+		for name, originalResource := range originalResources {
+			if mock, ok := copyTestCase.request.resourceMocks[name]; ok {
+				mock.AssertExpectations(t)
+			}
+			endpoint.Config.Resources[name] = originalResource
+		}
+	}
+
+	for header, value := range copyTestCase.headers {
+		actualHeaderValue := requestCtx.Response.Header.Peek(header)
+		if !bytes.Equal(actualHeaderValue, []byte(value)) {
+			t.Errorf("expected header %v = %v , but got %v = %v", header, value, header, actualHeaderValue)
+		}
+	}
+
+	if copyTestCase.httpStatus != actualHttpStatus {
+		t.Errorf("expected http status %v, but got %v", copyTestCase.httpStatus, actualHttpStatus)
+	}
+
+	if copyTestCase.emptyBody {
+		if !bytes.Equal(nil, actualResponseBody) {
+			t.Errorf("expected empty response body, but got '%v'", string(actualResponseBody))
+		}
+	} else {
+		if requestCtx.Response.Header.ContentType() == nil {
+			t.Errorf("response is missing header Content-Type")
+		} else if !bytes.Equal(requestCtx.Response.Header.ContentType(), []byte(copyTestCase.responseMimeType)) {
+			t.Errorf("expected response mime type '%v', but got '%v'", copyTestCase.responseMimeType, requestCtx.Response.Header.ContentType())
+		}
+
+		if !bytes.Equal(copyTestCase.responseBody, actualResponseBody) {
+			t.Errorf("expected response body '%v', but got '%v'", string(copyTestCase.responseBody), string(actualResponseBody))
+		}
+	}
+
+	requestFuzzTest(t, testCase.server, testCase.request.httpMethod, testCase.request.path)
 }
 
 func requestFuzzTest(t *testing.T, server *server.Server, httpMethod string, path string) {
--- server/servertest/request_test.go
+++ server/servertest/request_test.go
@@ -10,30 +10,34 @@ func Test_RouteTest(t *testing.T) {
 
 	sample := routes()
 
-	TestRequest(t, sample, "Route not found",
+	TestCase(t, sample, "Route not found").
 		GivenRequest(http.MethodPost, "/not_found").
-			WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
-			ExpectResponse(http.StatusNotFound).
-			WithEmptyBody())
+		WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
+		ExpectResponse(http.StatusNotFound).
+		WithEmptyBody().
+		Run()
 
-	TestRequest(t, sample, "Success POST /sample",
+	TestCase(t, sample, "Success POST /sample").
 		GivenRequest(http.MethodPost, "/sample").
-			WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
-			ExpectResponse(http.StatusCreated).
-			WithResponseBody("application/json", []byte(`{"output_string":"hello","output_int":5}`)))
+		WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
+		ExpectResponse(http.StatusCreated).
+		WithResponseBody("application/json", []byte(`{"output_string":"hello","output_int":5}`)).
+		Run()
 
 	dbMock := &mockDatastore{}
 	dbMock.On("RetrieveValue").Return("test")
-	TestRequest(t, sample, "Success GET /sample/{id}",
+	TestCase(t, sample, "Success GET /sample/{id}").
 		GivenRequest(http.MethodGet, "/sample/{id}").
-			WithPathParam("id", "34").
-			WithResourceMock("datastore", dbMock).
-			ExpectResponse(http.StatusOK).
-			WithResponseBody("application/json", []byte(`{"output_string":"test","output_int":34}`)))
+		WithPathParam("id", "34").
+		WithResourceMock("datastore", dbMock).
+		ExpectResponse(http.StatusOK).
+		WithResponseBody("application/json", []byte(`{"output_string":"test","output_int":34}`)).
+		Run()
 
 	// Not mocked, so it returns 500.
-	TestRequest(t, sample, "Failure, not mocked",
+	TestCase(t, sample, "Failure, not mocked").
 		GivenRequest(http.MethodGet, "/sample/{id}").
-			ExpectResponse(http.StatusInternalServerError).
-			WithResponseBody("application/json", []byte(`{"message":"[SW105] internal server error"}`)))
+		ExpectResponse(http.StatusInternalServerError).
+		WithResponseBody("application/json", []byte(`{"message":"[SW105] internal server error"}`)).
+		Run()
 }
