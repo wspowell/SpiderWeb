5185e9646a4a61504708e47c973aadf7ba6bda58
Wesley Powell <wesley.scott.powell@gmail.com>
2021-10-09 23:13:37 +0000

    Updated routes testing pattern

--- server/restful/restfultest/request_test.go
+++ server/restful/restfultest/request_test.go
@@ -8,39 +8,49 @@ import (
 	"github.com/wspowell/spiderweb/test"
 )
 
-func Test_RouteTest(t *testing.T) {
+func Test_RouteNotFound(t *testing.T) {
 	t.Parallel()
 
-	sample := Routes()
-
-	restfultest.TestCase(sample, "Route not found").
+	restfultest.TestCase(Routes(), "Route not found").
 		GivenRequest(http.MethodPost, "/not_found").
 		WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
 		ExpectResponse(http.StatusNotFound).
 		WithEmptyBody().
-		RunParallel(t)
+		Run(t)
+}
 
-	restfultest.TestCase(sample, "Success POST /sample").
+func Test_POST_sample(t *testing.T) {
+	t.Parallel()
+
+	restfultest.TestCase(Routes(), "Success POST /sample").
 		GivenRequest(http.MethodPost, "/sample").
 		WithRequestBody("application/json", []byte(`{"my_string": "hello","my_int": 5}`)).
 		ExpectResponse(http.StatusCreated).
 		WithResponseBody("application/json", []byte(`{"output_string":"hello","output_int":5}`)).
-		RunParallel(t)
+		Run(t)
+}
+
+func Test_POST_sample_id_34(t *testing.T) {
+	t.Parallel()
 
 	dbMock := &test.MockDatastore{}
 	dbMock.On("RetrieveValue").Return("test")
-	restfultest.TestCase(sample, "Success GET /sample/{id}").
+	restfultest.TestCase(Routes(), "Success GET /sample/{id}").
 		GivenRequest(http.MethodGet, "/sample/{id}").
 		WithPathParam("id", "34").
 		WithResourceMock("datastore", dbMock).
 		ExpectResponse(http.StatusOK).
 		WithResponseBody("application/json", []byte(`{"output_string":"test","output_int":34}`)).
-		RunParallel(t)
+		Run(t)
+}
+
+func Test_resource_not_mocked(t *testing.T) {
+	t.Parallel()
 
 	// Not mocked, so it returns 500.
-	restfultest.TestCase(sample, "Failure, not mocked").
+	restfultest.TestCase(Routes(), "Failure, not mocked").
 		GivenRequest(http.MethodGet, "/sample/{id}").
 		ExpectResponse(http.StatusInternalServerError).
 		WithResponseBody("application/json", []byte(`{"message":"[SW001] internal server error"}`)).
-		RunParallel(t)
+		Run(t)
 }
